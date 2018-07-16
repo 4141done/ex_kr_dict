04748231ce74f095356b2cbd9b1010187d50e4d4
Javier <jevans@avvo.com>
2018-07-16 09:11:59 -0700

    Added test cases for other requirements not yet met by current strategy

--- test/trie_test.exs
+++ test/trie_test.exs
@@ -6,38 +6,51 @@ defmodule TrieTest do
     %{trie: trie}
   end
 
-  test "can insert a word", %{trie: trie} do
+  test "insert/2 can insert a word", %{trie: trie} do
     Trie.insert(trie, "공기")
   end
 
-  test "can insert another word with same initial", %{trie: trie} do
+  test "insert/2 can insert another word with same initial", %{trie: trie} do
     Trie.insert(trie, "공기")
     Trie.insert(trie, "공기밥")
   end
 
-  test "can insert another word with different initial", %{trie: trie} do
+  test "insert/2 can insert another word with different initial", %{trie: trie} do
     Trie.insert(trie, "공기")
     Trie.insert(trie, "광화문")
   end
 
-  test "can find inserted word", %{trie: trie} do
+  test "find/2 can find inserted word", %{trie: trie} do
     Trie.insert(trie, "공기")
     Trie.insert(trie, "광화문")
 
     assert {:ok, "공기"} == Trie.find(trie, "공기")
   end
 
-  test "will not find anything input does not match inserted initial", %{trie: trie} do
+  test "find/2 will not find anything input does not match inserted initial", %{trie: trie} do
     Trie.insert(trie, "공기")
     Trie.insert(trie, "광화문")
 
     assert :not_found = Trie.find(trie, "고양이")
   end
 
-  test "will not find anything if input matches non-intial syllable", %{trie: trie} do
+  test " find/2will not find anything if input matches non-intial syllable", %{trie: trie} do
     Trie.insert(trie, "공기")
     Trie.insert(trie, "광화문")
 
     assert :not_found = Trie.find(trie, "기")
+  end
+
+  test "find/2 will not match partial non-words", %{trie: trie} do
+    Trie.insert(trie, "광화문")
+    assert :not_found == Trie.find(trie, "광")
+  end
+
+  test "prefix/2 find any words that share a prefix", %{trie: trie} do
+    Trie.insert(trie, "공기")
+    Trie.insert(trie, "공기밥")
+    Trie.insert(trie, "공항버스")
+
+    assert {:ok, ["공기", "공기밥", "공항버스"]} = Trie.prefix(trie, "공")
   end
 end
