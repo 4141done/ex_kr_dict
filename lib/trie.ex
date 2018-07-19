ca521655ae167037880fe569519c80f88e90b0e2
Javier <jevans@avvo.com>
2018-07-19 05:47:50 -0700

    trie now uses stop words to do looking

--- lib/trie.ex
+++ lib/trie.ex
@@ -1,7 +1,7 @@
 defmodule Trie do
   use GenServer
 
-  defstruct children: %{}, value: nil, root: nil
+  defstruct children: %{}, value: nil, is_word_boundary: false
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, opts)
@@ -27,15 +27,12 @@ defmodule Trie do
     GenServer.call(pid, :barf)
   end
 
-  def handle_call(:barf, _from, state) do
-    {:reply, state, state}
-  end
-
   def handle_call({:find, binary}, _from, state) do
     {:reply, do_find(state, [], binary), state}
   end
 
   def handle_call({:prefix, binary}, _from, state) do
+    {:reply, do_find(state, [], binary), state}
   end
 
   def handle_cast({:insert, binary}, state) do
@@ -43,29 +40,43 @@ defmodule Trie do
     {:noreply, new_state}
   end
 
-  # defp find_after(%{children: children} = current_node, found) do
-  #   children
-  #   |> Enum.map(fn (key, val) ->
-  #   end)
-  # end
+  def handle_call(:barf, _from, state) do
+    {:reply, state, state}
+  end
+
+  defp do_find(%{is_word_boundary: true}, found, ""), do: {:ok, List.to_string(found)}
+
+  defp do_find(%{is_word_boundary: false}, _found, ""), do: :not_found
 
   defp do_find(nil, _, _), do: :not_found
-  defp do_find(_current_node, found, ""), do: {:ok, List.to_string(found)}
-  defp do_find(%{children: children} = current_node, found, binary) do
-    case Map.get(children, binary) do
+
+  defp do_find(%{children: children} = current_node, found, <<current::utf8>> <> rest) do
+    case Map.get(children, current) do
       nil ->
-        do_find(nil, found, binary)
-      %{value: val} ->
-        {:ok, val}
+        do_find(nil, found, rest)
+      %{value: val} = next_node ->
+        IO.puts "-> #{val}"
+        do_find(next_node, found ++ [val], rest)
     end
   end
 
-  defp do_insert(%{children: children} = current_node, root, path, binary) do
-    case Map.get(children, binary) do
+  defp do_insert(_current_node, root, _path, ""), do: root
+
+  defp do_insert(%{children: children} = current_node, root, path, <<current::utf8>> <> rest) do
+    case Map.get(children, current) do
       nil ->
-        put_in(root, path ++ [binary], %{value: binary, children: %{}})
+        IO.puts("new node")
+        IO.inspect(path)
+        new_node = %{
+          value: current,
+          children: %{},
+          is_word_boundary: String.length(rest) == 0
+        }
+        do_insert(new_node, put_in(root, path ++ [current], new_node), path ++ [current, :children], rest)
       child ->
-        do_insert(child, root, path ++ [binary, :children], binary)
+        IO.puts("found child")
+        IO.inspect(path)
+        do_insert(child, root, path ++ [current, :children], rest)
     end
   end
 end
